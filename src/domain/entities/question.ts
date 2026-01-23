d41c0fb663cb9db492f0d3ccbc9efb8082590ccf
gabriel-pivoto <gabrielspivoto@gmail.com>
2026-01-23 13:39:22 -0300

    dependencia externa

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+interface QuestionProps {
+    title: string;
+    content: string;
+    authorId: string;
+}
+
 export class Question {
     public title: string;
     public id: string;
     public content: string;
+    public authorId: string;
 
-    constructor(title: string, content: string, id?: string) {
-        this.title = title;
+    constructor(props: QuestionProps, id?: string) {
+        this.content = props.content;
+        this.authorId = props.authorId;
+        this.title = props.title;
         this.id = id ?? randomUUID();
-        this.content = content;
     }
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,14 +1,26 @@
-import { expect, test } from "vitest"
-import { AnswerQuestionUseCase } from "./answer-question"
+import { expect, test } from "vitest";
+import { Answer } from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { AnswerQuestionUseCase } from "./answer-question";
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+class InMemoryAnswersRepository implements AnswersRepository {
+  public items: Answer[] = [];
 
-  const answer = answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'Nova resposta',
-  })
+  async create(answer: Answer): Promise<void> {
+    this.items.push(answer);
+  }
+}
 
-  expect(answer.content).toEqual('Nova resposta')
-})
+test("create an answer", async () => {
+  const answersRepository = new InMemoryAnswersRepository();
+  const answerQuestion = new AnswerQuestionUseCase(answersRepository);
+
+  const answer = await answerQuestion.execute({
+    questionId: "1",
+    instructorId: "1",
+    content: "Nova resposta",
+  });
+
+  expect(answer.content).toEqual("Nova resposta");
+  expect(answersRepository.items[0]).toEqual(answer);
+});
