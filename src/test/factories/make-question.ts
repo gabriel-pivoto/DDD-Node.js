599f04d2b93ce005353b3fba5d91428c8a3f18fa
gabriel-pivoto <gabrielspivoto@gmail.com>
2026-01-26 15:59:04 -0300

    faker datas

--- src/test/factories/make-question.ts
+++ src/test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const newQuestion = Question.create({
-    authorId: new UniqueEntityID(),
-    content: 'Example content',
-    slug: Slug.create('example-question'),
-    title: 'Example Question',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const newQuestion = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      slug: Slug.create('example-question'),
+      title: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return newQuestion
 }
