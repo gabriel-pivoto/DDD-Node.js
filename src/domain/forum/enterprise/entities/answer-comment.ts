6663c1263fc871935ff9bce6f9d8cf6a6f1bf482
gabriel-pivoto <gabrielspivoto@gmail.com>
2026-01-30 10:54:47 -0300

    começando aggragate

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
-import { Comment, CommentProps } from '@/domain/forum/enterprise/entities/comment'
+import {
+  Comment,
+  CommentProps,
+} from '@/domain/forum/enterprise/entities/comment'
 
 export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,4 +1,4 @@
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
@@ -14,7 +14,7 @@ export interface QuestionProps {
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get title() {
     return this.props.title
   }
